95ee94d6fcdfabcd9aba5fbc8dd56e8b2eb7c59e
Rosay Jeremy <rosay.jeremy@gmail.com>
2021-08-30 18:00:39 +0200

    WIP Design

--- js/cart.js
+++ js/cart.js
@@ -48,7 +48,7 @@ function displayCartItems(productInSessionStorage) {
 	let cartContainer = document.getElementById("cartContainer");
 	// Si le panier contient des produits, il les affiche
 	if (productInSessionStorage.length > 0) {
-		for (let product of productInSessionStorage) {
+		productInSessionStorage.forEach((product) => {
 			// Crée la div qui contient chaque produit
 			const cartItem = document.createElement("div");
 			cartItem.setAttribute("class", "row p-3 align-items-center border-bottom");
@@ -87,13 +87,13 @@ function displayCartItems(productInSessionStorage) {
 
 			// Affiche le bouton "-"
 			const cartProductQuantitySub = document.createElement("button");
-			cartProductQuantitySub.setAttribute("class", "subButton col-2 col-md-2 text-center");
+			cartProductQuantitySub.setAttribute("class", "subButton col-2 col-md-3 text-center");
 			cartProductQuantitySub.innerHTML = "-";
 			cartProductQuantityContainer.appendChild(cartProductQuantitySub);
 
 			// Affiche la quantité
 			const cartProductQuantity = document.createElement("input");
-			cartProductQuantity.setAttribute("class", "quantity col-2 col-md-6 text-center");
+			cartProductQuantity.setAttribute("class", "quantity col-2 col-md-3 text-center");
 			cartProductQuantity.setAttribute("value", `${product.quantity}`);
 			cartProductQuantity.setAttribute("name", "qty");
 			cartProductQuantity.setAttribute("id", `quantity-${product.id}-${product.lenses}`);
@@ -102,7 +102,7 @@ function displayCartItems(productInSessionStorage) {
 
 			// Affiche le bouton "+"
 			const cartProductQuantityAdd = document.createElement("button");
-			cartProductQuantityAdd.setAttribute("class", "addButton col-2 col-md-2 text-center");
+			cartProductQuantityAdd.setAttribute("class", "addButton col-2 col-md-3 text-center");
 			cartProductQuantityAdd.innerHTML = "+";
 			cartProductQuantityContainer.appendChild(cartProductQuantityAdd);
 
@@ -119,22 +119,23 @@ function displayCartItems(productInSessionStorage) {
 			cartItem.appendChild(divRemoveButton);
 			// Crée le bouton poubelle
 			const removeButton = document.createElement("button");
-			removeButton.setAttribute("class", "removeButton btn bg-pink btn-outline-dark pl-4 pr-4");
+			removeButton.setAttribute("class", "removeButton border btn pl-4 pr-4");
 			removeButton.setAttribute("data-item", product.id);
 			removeButton.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-            </svg>`;
+  			<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  			<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+			</svg>`;
 			divRemoveButton.appendChild(removeButton);
 
 			//Affiche le prix total du panier
 			const totalCartPrice = document.getElementById("totalCartPrice");
 			totalCartPrice.innerHTML = totalCost;
-		}
+		});
 	}
 
 	//****************************************CREATION BOUTON INCREMENTATION ET DECREMENTATION*************
 	let substractButton = document.getElementsByClassName("subButton");
+
 	let addButton = document.getElementsByClassName("addButton");
 	let products = JSON.parse(sessionStorage.getItem("products"));
 	let totalCostInSessionStorage = JSON.parse(sessionStorage.getItem("totalCost"));
@@ -176,6 +177,7 @@ function displayCartItems(productInSessionStorage) {
 
 			input.value = newValue;
 			products[i].quantity = input.value;
+
 			totalCostInSessionStorage = totalCostInSessionStorage - products[i].price;
 			sessionStorage.setItem("products", JSON.stringify(products));
 			sessionStorage.setItem("totalCost", JSON.stringify(totalCostInSessionStorage));
